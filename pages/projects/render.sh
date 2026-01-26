875b1e52cd756fbe86f6c65a1bcfd230a8d1d30e
David Krasowska <krasow@u.northwestern.edu>
2026-01-25 21:41:47 -0600

    /polish

--- pages/projects/render.sh
+++ pages/projects/render.sh
@@ -1,3 +1,6 @@
 pandoc -f markdown -t html5 --no-highlight cunumeric.md -o cunumeric.html --mathjax --metadata title="cunumeric"
 pandoc -f markdown -t html5 --no-highlight legionpim.md -o legionpim.html --mathjax --metadata title="legionpim"
 pandoc -f markdown -t html5 --no-highlight compression.md -o compression.html --mathjax --metadata title="compression"
+
+# for polish project
+pandoc -f markdown -t html5 --no-highlight polish.md -o polish.html --mathjax --metadata title="polish"
